94f40cfbb71096b82d714483cec7428f4eaee739
jeevaggithub <jeevagopal2000@gmail.com>
2023-05-26 19:41:34 +0530

    communication between components.

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -9,12 +9,15 @@ import { recipeModel } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes:recipeModel[] = [
-    new recipeModel('this is a sample1 name ','this is a sample1 description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+    new recipeModel('this is a sample1 name ','this is a sample1 description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    new recipeModel('this is a sample2 name ','this is a sample2 description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+    // console.log('recipeModel',this.recipes);
+    // console.log('recipeModel',this.recipes[0].name);
   }
 
 }
--- src/app/components/shoping-list/shoping-list.component.ts
+++ src/app/components/shoping-list/shoping-list.component.ts
@@ -1,14 +1,23 @@
 import { Component,OnInit } from "@angular/core";
+import { Ingredients } from "src/app/shared/ingredients.model";
 
 @Component({
   selector:'app-shoping-list',
   templateUrl:'shoping-list.component.html',
   styleUrls:['./shoping-list.component.scss']
 })
-export class shopingListComponent {
+export class shopingListComponent implements OnInit {
+
+  ingredients :Ingredients[]=[
+    new Ingredients('Apple',5),
+    new Ingredients('Tomato',10),
+    new Ingredients('Onion',15),
+  ];
 
   constructor(){}
 
-
+  ngOnInit(): void {
+    console.log('ingredients',this.ingredients);
+  }
 
 }
